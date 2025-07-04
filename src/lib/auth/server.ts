6fc9d030ada681cc2a138d23bf66dccfb37f7acc
austin-smith <austinsmith23@gmail.com>
2025-07-04 16:20:35 -0700

    Extend logout timer (just use Better Auth defaults)

--- src/lib/auth/server.ts
+++ src/lib/auth/server.ts
@@ -144,7 +144,7 @@ export const auth = betterAuth.betterAuth({
       })
   ],
   session: {
-    expiresIn: 60 * 60 * 24 * 1,
-    updateAge: 60 * 60 * 6,
+    expiresIn: 60 * 60 * 24 * 7, // 7 days
+    updateAge: 60 * 60 * 24 // 1 day (every 1 day the session expiration is updated)
   },
 });
